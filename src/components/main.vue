8f62b0993352704e3a3eef2d9d9c02d04ff5aaae
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-26 15:12:38 +0200

    created professionalCard.vue components

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,18 +1,23 @@
 <template>
   <section>
-      <infoCourse/>
-      <statiscCard/>
+      <section>
+            <infoCourse/>
+            <statiscCard/>
+            <professionalCard/>
+      </section>
   </section>
 </template>
 
 <script>
 import infoCourse from './infoCourse.vue' 
 import statiscCard from './statiscCard.vue' 
+import professionalCard from './professionalCard.vue' 
 
 export default {
       components :{
          infoCourse,
          statiscCard,
+         professionalCard
        
     },
 }
