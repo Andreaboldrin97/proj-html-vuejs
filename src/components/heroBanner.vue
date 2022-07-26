12e7d8ee6a9f771a196bb989ad7d4d8fd1975871
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-26 14:15:31 +0200

    change overflow banner-container  on heroBannervue

--- src/components/heroBanner.vue
+++ src/components/heroBanner.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="d-flex justify-content-center position-relative banner-container">
-      <div class="d-flex align-items-end">
-          <img src="../assets/remote-training/images/home-6-hero-left-person-244x300.png" alt="">
+  <div class="container d-flex  position-relative banner-container ">
+      <div class="d-flex align-items-end position-relative hero-left">
+          <img src="../assets/remote-training/images/home-6-hero-left-person.png" alt="">
       </div>
-      <div class="position-relative border-hero">
+      <div class="position-relative hero-center mb-4">
             <img src="../assets/remote-training/images/home-6-hero-poster-final.jpg" alt="">
-          <div class="position-absolute top-50 start-50 img-play">
-               <img src="../assets/remote-training/images/icon-youtube-play.png" alt="">
-          </div>
+            <div class="position-absolute top-50 start-50 img-play">
+                <img src="../assets/remote-training/images/icon-youtube-play.png" alt="">
+            </div>
       </div>
-      <div class="d-flex align-items-end">
+      <div class="d-flex align-items-end position-relative hero-right">
            <img src="../assets/remote-training/images/home-6-hero-right-person-195x300.png" alt="">
       </div>
   </div>
@@ -18,21 +18,37 @@
 <script>
 export default {
 
+
 }
 </script>
 
 <style lang="scss" scoped>
+
 img{
     width: 100%;
 }
 .banner-container{
-    top: 60px;
+    right: 5%;
+    top: 70px;
+    overflow: hidden;
 }
-.border-hero{
+.hero-left{
+    left: 40px;
+}
+.hero-center{
     border: 12px solid white;
     border-radius: 10px;
+    box-shadow: 5px 5px 20px rgb(142, 141, 141);
+    img{
+        object-fit: cover;
+        object-position: top;
+    }
+        .img-play{
+            transform: translate(-50% , -50%);
+        }
 }
-.img-play{
-    transform: translate(-50% , -50%);
+.hero-right{
+    top: 114px;
 }
+
 </style>
