5ce17ebc24dc5dd8a6a07ba1f61dcd258f86f66a
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-26 18:21:45 +0200

    created sectionPrice.vue

--- src/components/main.vue
+++ src/components/main.vue
@@ -5,6 +5,9 @@
             <statiscCard/>
             <professionalSection/>
       </section>
+      <section class="price">
+          <sectionPrice/>
+      </section>
   </main>
 </template>
 
@@ -12,19 +15,25 @@
 import infoCourse from './infoCourse.vue' 
 import statiscCard from './statiscCard.vue' 
 import professionalSection from './professionalSection.vue' 
+import sectionPrice from './sectionPrice.vue' 
 
 export default {
       components :{
          infoCourse,
          statiscCard,
-         professionalSection
-       
+         professionalSection,
+       sectionPrice
     },
 }
 </script>
 
 <style lang="scss">
+@import "../style/variables.scss";
 .info{
     background-image: url('../assets/remote-training/images/background-pattern-grid-line.png');
+}
+.price {
+    margin-top: 150px ;
+    background-color: $bgSectionPrice;
 }
 </style>
