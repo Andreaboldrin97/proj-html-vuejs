d4eee7dd476ba2e471e8a43c25fc9aef5fb87dbe
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-27 15:25:22 +0200

    change font pn text-title on professionalCard.vue

--- src/components/professionalCard.vue
+++ src/components/professionalCard.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="professional-card m-3 my-3 px-2 d-flex flex-column justify-content-between " :class="isReverse  ? 'flex-column-reverse' : '' ">
         <div class="p-2 img-text-card">
-                <div>{{ profession }}</div>
+                <div class="text-title pt-3">{{ profession }}</div>
                 <div class="text-description pt-1">
                   {{ description }}
                 </div>
@@ -34,6 +34,10 @@ export default {
     height: 200px;
     object-fit: contain;
     object-position: left;
+  }
+  .text-title{
+    font-size: 20px;
+    font-weight: 500;
   }
   .text-description{
     font-size: 20px;
