a66598b736b3f3c0940d6d24cbfc6c60c418bd0a
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-27 10:21:13 +0200

    create form blogCard.vue

--- src/components/blogCard.vue
+++ src/components/blogCard.vue
@@ -1,33 +1,56 @@
 <template>
-  <div class="blog-card">
-      <div>
-          <img src="" alt="">
-      </div>
-      <div class="text-card">
-          <div class="genre">
 
-          </div>
-          <div class="blog-title">
-
-          </div>
+    <div class="blog-card">
+        <div>
+            <img class="rounded" src="../assets/remote-training/images/artist-blog-01-480x325.jpg" alt="">
+        </div>
+        <div class="text-card">
+            <div class="genre py-3">
+                ARTIST
+            </div>
+            <div class="blog-title pb-4">
+                Pocket-Sized Notebooks Hold Miniature Paintings
+            </div>
             <div class="d-flex blog-views">
-                  <div class="pe-5">
-                      <i class="fa-regular fa-file-lines pe-2"></i>
-                      {{ lessons }} Lessons
-                  </div>
-                  <div>
-                      <i class="fa-regular fa-user pe-2"></i>
-                      {{ students }} Students
-                  </div>
-              </div>
-      </div>
-  </div>
+                <div class="pe-5">
+                    <i class="fa-regular fa-calendar pe-2"></i>
+                        May 15,2020
+                </div>
+                <div>
+                    <i class="fa-regular fa-eye"></i>
+                        views
+                </div>
+            </div>
+        </div>
+    </div>
+     
+
 </template>
 
 <script>
 
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+.blog-card {
+    width: calc((100vw / 3) - 30px);
+    margin: 15px;
+    img{
+        width: 100%;
+    }
+}
+.text-card{
+    .genre{
+        color: $colorTextFocus;
+        font-weight: 400;
+    }
+    .blog-title{
+        font-size: 20px;
+        font-weight: 700;
+    }
+    .blog-views{
+        color: $colorTextFocus;
+    }
+}
 </style>
